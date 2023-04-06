92aa9acb2a90ba1a2d4d1bfa8d74da9fcf533876
MesakDuduCoder <duduaoa99@gmail.com>
2023-04-07 02:28:33 +0530

    Fix edit bug

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function component() {
   <form id="add-task-form">
     <input type="text" placeholder="Add to your list..." id="add-task"/>
   </form>
-  <button class="submit-task">
+  <button id="submit-task">
   <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="30px" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="M16,13h-6v-3l-5,4l5,4v-3h7c0.553,0,1-0.447,1-1V5h-2V13z"></path></svg>
   </button>`;
   addTask.style = 'padding: 0 10px;';
